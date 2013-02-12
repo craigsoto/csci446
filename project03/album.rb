5cec419f4aef79f1af7c5eae157294521014fcf3
Craig Soto <csoto@mines.edu>
2013-02-11 20:54:16 -0700

    submitting files for project 3 part 1

--- project03/album.rb
+++ project03/album.rb
@@ -2,10 +2,10 @@ class Album
 
   attr_accessor :rank, :title, :year
 
-  def initialize(rank, raw_string)
+  def initialize(title, year, rank)
     @rank = rank
-    @title, raw_year = raw_string.split(',')
-    @year = raw_year[/\d+/].to_i
+    @title = title
+    @year = year
   end
 
 end
