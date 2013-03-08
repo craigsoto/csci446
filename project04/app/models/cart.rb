133d95a96e439f7b1b272b63107322515a3ee0ab
Craig Soto <csoto@mines.edu>
2013-03-08 01:34:38 -0700

    committing the combine items in cart changes for up and down migration

--- project04/app/models/cart.rb
+++ project04/app/models/cart.rb
@@ -1,6 +1,6 @@
 class Cart < ActiveRecord::Base
 	has_many :line_items, :dependent => :destroy
-	
+	attr_accessible :cart_id
 	def add_product(product_id)
 		current_time = line_items.find_by_product_id(product_id)
 		if current_time
--- project04/app/models/line_item.rb
+++ project04/app/models/line_item.rb
@@ -1,4 +1,5 @@
 class LineItem < ActiveRecord::Base
 	belongs_to :product
 	belongs_to :cart
+	attr_accessible :product_id, :quantity, :cart_id
 end
