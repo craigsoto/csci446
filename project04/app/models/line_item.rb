266aaf5c75744a714bf22ee68855e924f371cbdb
Craig Soto <csoto@mines.edu>
2013-03-08 02:04:05 -0700

    committing changes to enhance the apperance to the cart

--- project04/app/models/line_item.rb
+++ project04/app/models/line_item.rb
@@ -2,4 +2,8 @@ class LineItem < ActiveRecord::Base
 	belongs_to :product
 	belongs_to :cart
 	attr_accessible :product_id, :quantity, :cart_id
+	
+	def total_price
+		product.price * quantity
+	end
 end
