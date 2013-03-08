992db58095c98f863d2e1a07ce29a9767808072f
Craig Soto <csoto@mines.edu>
2013-03-08 01:02:16 -0700

    committing files for the 'smart' cart

--- project04/app/models/cart.rb
+++ project04/app/models/cart.rb
@@ -1,3 +1,13 @@
 class Cart < ActiveRecord::Base
 	has_many :line_items, :dependent => :destroy
+	
+	def add_product(product_id)
+		current_time = line_items.find_by_product_id(product_id)
+		if current_time
+			current_time.quantity += 1
+		else
+			current_time = line_items.build(:product_id => product_id)
+		end
+		current_time
+	end
 end
